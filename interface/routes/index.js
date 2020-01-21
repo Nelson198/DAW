6f3695bef1e3db90e07ed36e46f6c889253d130c
JRI98 <38755101+JRI98@users.noreply.github.com>
2020-01-21 17:26:38 +0000

    Me and I

--- interface/routes/index.js
+++ interface/routes/index.js
@@ -4,8 +4,8 @@ const passport = require("passport")
 const bcrypt = require("bcryptjs")
 let router = express.Router()
 
-let verificaAutenticacao = (req,res,next) => {
-    if(req.isAuthenticated()) {
+let verificaAutenticacao = (req, res, next) => {
+    if (req.isAuthenticated()) {
         // req.isAuthenticated() will return true if user is logged in
         next()
     } else {
@@ -15,46 +15,46 @@ let verificaAutenticacao = (req,res,next) => {
 
 router.get("/", verificaAutenticacao, (req, res) => {
     axios.get("http://localhost:5003")
-        .then(dados => res.render("index", {lista: dados.data}))
-        .catch(e => res.render("error", {error: e}))
+        .then(dados => res.render("index", { lista: dados.data }))
+        .catch(e => res.render("error", { error: e }))
 })
 
-router.get("/eventos/:id", verificaAutenticacao, (req,res) => {
+router.get("/eventos/:id", verificaAutenticacao, (req, res) => {
     axios.get("http://localhost:5003/eventos/" + req.params.id)
-        .then(dados => res.render("evento", {evento: dados.data}))
-        .catch(e => res.render("error", {error: e}))
+        .then(dados => res.render("evento", { evento: dados.data }))
+        .catch(e => res.render("error", { error: e }))
 })
 
-router.get("/logout", verificaAutenticacao, (req,res) => {
+router.get("/logout", verificaAutenticacao, (req, res) => {
     req.logout()
     res.redirect("/")
 })
 
-router.get("/login", (req,res) => {
+router.get("/login", (req, res) => {
     res.render("login")
 })
 
-router.get("/register", (req,res) => {
+router.get("/register", (req, res) => {
     res.render("registo")
 })
 
-router.post("/login", passport.authenticate("local", { 
+router.post("/login", passport.authenticate("local", {
     successRedirect: "/",
     successFlash: "Utilizador autenticado com sucesso!",
     failureRedirect: "/login",
     failureFlash: "Utilizador ou password inválido(s)..."
-  })
+})
 )
 
-router.post("/reg", (req,res) => {
+router.post("/reg", (req, res) => {
     let hash = bcrypt.hashSync(req.body.password, 10)
     axios.post("http://localhost:5003/utilizadores", {
         email: req.body.email,
         nome: req.body.nome,
         password: hash
     })
-    .then(dados => res.redirect("/"))
-    .catch(e => res.render("error", {error: e}))
+        .then(dados => res.redirect("/"))
+        .catch(e => res.render("error", { error: e }))
 })
 
 module.exports = router
---
+++ requisitos.md
@@ -0,0 +1,11 @@
+Tema: Uminho ISN (Informatics Social Network)
+
+Requisitos:
+    - utilizadores;
+    - autenticação;
+    - publicações:
+        - texto;
+        - PDF's;
+        - hashtags;
+    - grupos;
+    - exportar/importar toda a base de dados.
